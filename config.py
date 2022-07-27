8ba3b4f31e5ca94ae6e1066ac9f0eb09318dc946
pxnx <loool1912@web.de>
2022-07-27 18:49:15 +0200

    fiddling

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ TOKEN = os.getenv("TOKEN")
 CHANNEL = -1001369053241
 
 NYX = 703453307
+
+api_id =int(os.getenv("ID"))
+api_hash = os.getenv("HASH")
--- main.py
+++ main.py
@@ -1,33 +1,45 @@
-import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, filters
+from telegram.ext import CommandHandler, MessageHandler
+from telethon import TelegramClient
 
-from telegram import ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Defaults
-
+import config
 from config import CHANNEL, TOKEN
-from messages import start, append_footer, join_member
+from messages import append_footer
 
+
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+    is_member = await check_member(update.message.from_user.id)
+
+    await update.message.reply_text(f"is member of i3: {is_member}")
+
+
+async def check_member(user_id):
+    async with TelegramClient("remove_inactive", config.api_id, config.api_hash) as client:
+
+        async for member in client.iter_participants("blog_itisinteresting"):
+            print(member.id)
+            if member.id == user_id: return True
+        return False
 
 def main():
-    updater = Updater(TOKEN, defaults=Defaults(parse_mode=ParseMode.HTML))
+    app = ApplicationBuilder().token(TOKEN).build()
 
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
 
-    dp.add_handler(
+
+    app.add_handler(
         MessageHandler(
-            Filters.update.channel_post &
-            (Filters.photo | Filters.video | Filters.animation)
-            & Filters.chat(chat_id=CHANNEL), append_footer))
+            (filters.PHOTO | filters.VIDEO | filters.ANIMATION)
+            & filters.Chat(chat_id=CHANNEL), append_footer))
 
-    dp.add_handler(MessageHandler(Filters.all, join_member))
+    app.add_handler(CommandHandler("start", hello))
 
-    updater.start_webhook(
-        "0.0.0.0",
-        int(os.environ["PORT"]),
-        TOKEN,
-        webhook_url=f"https://ptb-i3.herokuapp.com/{TOKEN}",
-    )
+    app.run_polling()
 
 
 if __name__ == '__main__':
     main()
+
