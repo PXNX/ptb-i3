eda2f54af0c62f1b8dd05226ba90ea703034be9d
pxnx <loool1912@web.de>
2022-07-27 16:14:17 +0200

    fiddling

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Defaults
 
 from config import CHANNEL, TOKEN
-from messages import start, append_footer
+from messages import start, append_footer, join_member
 
 
 def main():
@@ -19,12 +19,15 @@ def main():
             (Filters.photo | Filters.video | Filters.animation)
             & Filters.chat(chat_id=CHANNEL), append_footer))
 
+    dp.add_handler(MessageHandler(Filters.all, join_member))
+
     updater.start_webhook(
         "0.0.0.0",
         int(os.environ["PORT"]),
         TOKEN,
         webhook_url=f"https://ptb-i3.herokuapp.com/{TOKEN}",
     )
+
 
 if __name__ == '__main__':
     main()
--- messages.py
+++ messages.py
@@ -1,6 +1,8 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+from config import NYX
+
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hi!")
@@ -8,4 +10,8 @@ def start(update: Update, context: CallbackContext):
 
 def append_footer(update: Update, _: CallbackContext):
     original_caption = update.channel_post.caption_html_urled if update.channel_post.caption is not None else ''
-    update.channel_post.edit_caption(f"{original_caption}\n\n👨‍💻 Join <a href='https://t.me/blog_itisinteresting'>IT IS INTERESTING</a> <u>NOW</u> for more!")
+    update.channel_post.edit_caption(
+        f"{original_caption}\n\n👨‍💻 Join <a href='https://t.me/blog_itisinteresting'>IT IS INTERESTING</a> <u>NOW</u> for more!")
+
+def join_member(update: Update, context: CallbackContext):
+    context.bot.send_message(NYX, str(update.message.new_chat_members))
